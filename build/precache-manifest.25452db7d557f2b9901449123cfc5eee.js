4b511eb7e4b3d62a891dbf7432590e6686fc1287
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-11-07 15:36:46 -0800

    bolden app links

--- build/precache-manifest.25452db7d557f2b9901449123cfc5eee.js
+++ build/precache-manifest.25452db7d557f2b9901449123cfc5eee.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cea21517e990332f9ce54cca9e755834",
+    "revision": "5dde2dfd068dc59c3be42316f83d13e8",
     "url": "/gjportfolio/index.html"
   },
   {
-    "revision": "799c7a12f96f56d4dd13",
-    "url": "/gjportfolio/static/css/main.c657ccf4.chunk.css"
+    "revision": "12f60edf31c9e16a232a",
+    "url": "/gjportfolio/static/css/main.4354b2a8.chunk.css"
   },
   {
     "revision": "ad4808a76d27e6a80037",
     "url": "/gjportfolio/static/js/2.b3593275.chunk.js"
   },
   {
-    "revision": "799c7a12f96f56d4dd13",
+    "revision": "12f60edf31c9e16a232a",
     "url": "/gjportfolio/static/js/main.73bf88f5.chunk.js"
   },
   {
