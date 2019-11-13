61596360d7bc0839244f466e28f157ba56351ef1
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-11-12 17:42:31 -0800

    add js smooth scrolling for links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './style.css';
 
 function Navbar() {
+
+    var handleScrollToElement = (event) => {
+        var elmnt = document.getElementById(event.target.value);
+        elmnt.scrollIntoView({behavior: 'smooth'});
+      }
+
     return (
         <nav id="navbar">
             <div className="nav-wrapper">
                 <a href="/gjportfolio/" className="brand-logo">My Portfolio</a>
                 <ul className="right">
-                    <li><a href="#about">/ About</a></li>
-                    <li><a href="#skills">/ Skills</a></li>
-                    <li><a href="#gallery">/ Projects</a></li>
+                    <li><button value="about" onClick={handleScrollToElement}>/ About</button></li>
+                    <li><button value="skills" onClick={handleScrollToElement}>/ Skills</button></li>
+                    <li><button value="gallery" onClick={handleScrollToElement}>/ Projects</button></li>
                 </ul>
             </div>
         </nav>
