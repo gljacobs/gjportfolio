e97d67856a7f99e1f8d16605874795f46977e825
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-01-07 02:17:39 -0800

    Updates

--- precache-manifest.5afa0379f65f4db6df2d08fd8c96948c.js
+++ precache-manifest.5afa0379f65f4db6df2d08fd8c96948c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "921f0ddf3a68f54932ef427ac7079664",
+    "revision": "64eb3cdc1e73d09f3091ce0cee09b692",
     "url": "/gjportfolio/index.html"
   },
   {
-    "revision": "4675d69b6fb0d492f5e0",
+    "revision": "57bc531084c835a76f31",
     "url": "/gjportfolio/static/css/main.13ca2748.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gjportfolio/static/js/2.b3593275.chunk.js"
   },
   {
-    "revision": "4675d69b6fb0d492f5e0",
-    "url": "/gjportfolio/static/js/main.c8c3a181.chunk.js"
+    "revision": "57bc531084c835a76f31",
+    "url": "/gjportfolio/static/js/main.cbe02611.chunk.js"
   },
   {
     "revision": "1c56f1fd502e8f4fb49c",
