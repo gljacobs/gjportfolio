7b46a092c766c155db435c1756803beaa125852d
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-01-28 15:28:02 -0800

    update links on skills

---
+++ build/precache-manifest.dd7372088e4c8c1311714eec0f810f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d818f7c189456309d90c3f744d8c7d",
+    "url": "/gjportfolio/index.html"
+  },
+  {
+    "revision": "43739aed0d15448ad803",
+    "url": "/gjportfolio/static/css/main.4372c079.chunk.css"
+  },
+  {
+    "revision": "ad4808a76d27e6a80037",
+    "url": "/gjportfolio/static/js/2.b3593275.chunk.js"
+  },
+  {
+    "revision": "43739aed0d15448ad803",
+    "url": "/gjportfolio/static/js/main.6ddb24d5.chunk.js"
+  },
+  {
+    "revision": "1c56f1fd502e8f4fb49c",
+    "url": "/gjportfolio/static/js/runtime~main.50c7f30a.js"
+  }
+]);
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -14,16 +14,16 @@ class Skills extends React.Component {
                 <div className="container">
                     <h3>Skills and Technologies</h3>
                     <div className="carousel">
-                        <a className="carousel-item" href="#one!"><img src="https://www.tuannguyen.tech/wp-content/uploads/2019/01/react.png" /></a>
-                        <a className="carousel-item" href="#two!"><img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/full/nodejslogo.png" /></a>
-                        <a className="carousel-item" href="#three!"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png" /></a>
-                        <a className="carousel-item" href="#four!"><img src="https://s3-eu-west-1.amazonaws.com/devinterface-web/production/technologies/30/medium/mongodb111.png?1444285573" /></a>
-                        <a className="carousel-item" href="#five!"><img src="https://apxsoftwares.com/wp-content/uploads/2019/07/2-2.png" /></a>
-                        <a className="carousel-item" href="#six!"><img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png" /></a>
-                        <a className="carousel-item" href="#seven!"><img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png" /></a>
-                        <a className="carousel-item" href="#eight!"><img src="https://i.pinimg.com/originals/99/99/c1/9999c1a8c347b81c5cb5df967664c4c3.gif" /></a>
-                        <a className="carousel-item" href="#nine!"><img src="http://www.superone.in/superone.in/wp-content/uploads/2017/07/bootstrap-logo.png" /></a>
-                        <a className="carousel-item" href="#ten!"><img src="https://cdn.iconscout.com/icon/free/png-256/github-153-675523.png" /></a>
+                        <a className="carousel-item" href="https://reactjs.org/" target="_blank"><img src="https://www.tuannguyen.tech/wp-content/uploads/2019/01/react.png" /></a>
+                        <a className="carousel-item" href="https://nodejs.org/" target="_blank"><img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/full/nodejslogo.png" /></a>
+                        <a className="carousel-item" href="https://developer.mozilla.org/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png" /></a>
+                        <a className="carousel-item" href="https://www.mongodb.com/" target="_blank"><img src="https://s3-eu-west-1.amazonaws.com/devinterface-web/production/technologies/30/medium/mongodb111.png?1444285573" /></a>
+                        <a className="carousel-item" href="https://www.w3schools.com/js/" target="_blank"><img src="https://apxsoftwares.com/wp-content/uploads/2019/07/2-2.png" /></a>
+                        <a className="carousel-item" href="https://www.w3schools.com/html/html5_intro.asp" target="_blank"><img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png" /></a>
+                        <a className="carousel-item" href="https://www.w3schools.com/css/" target="_blank"><img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png" /></a>
+                        <a className="carousel-item" href="https://jquery.com/" target="_blank"><img src="https://i.pinimg.com/originals/99/99/c1/9999c1a8c347b81c5cb5df967664c4c3.gif" /></a>
+                        <a className="carousel-item" href="https://getbootstrap.com/" target="_blank"><img src="http://www.superone.in/superone.in/wp-content/uploads/2017/07/bootstrap-logo.png" /></a>
+                        <a className="carousel-item" href="https://github.com/" target="_blank"><img src="https://cdn.iconscout.com/icon/free/png-256/github-153-675523.png" /></a>
                     </div>
                 </div>
             </div>
