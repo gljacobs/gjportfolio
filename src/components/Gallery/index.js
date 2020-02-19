715eb5e1abb12809752bfa81dbb1bf00ab6dc64a
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-02-19 12:55:13 -0800

    add approp links to anchors

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -8,91 +8,91 @@ function Gallery() {
                 <h3>My Projects</h3>
                 <div className="row">
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/camp.png" alt="Camp Dad" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/project1/"><img src="./images/camp.png" alt="Camp Dad" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/project1/">Camp Dad</a>
                             <a className="gitlink" href="https://github.com/gljacobs/project1" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/gbooks.png" alt="GBooks-Search" /></a>
+                        <a target="_blank" href="https://limitless-reaches-94201.herokuapp.com/"><img src="./images/gbooks.png" alt="GBooks-Search" /></a>
                         <h5><a target="_blank" href="https://limitless-reaches-94201.herokuapp.com/">Book Search</a>
                             <a className="gitlink" href="https://github.com/gljacobs/google-books-search" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/ccv1.png" alt="ChitChatv1" /></a>
+                        <a target="_blank" href="https://chitchatv1.herokuapp.com/"><img src="./images/ccv1.png" alt="ChitChatv1" /></a>
                         <h5><a target="_blank" href="https://chitchatv1.herokuapp.com/">ChitChat</a>
                             <a className="gitlink" href="https://github.com/gljacobs/chitchat" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/ezchat.png" alt="EZ Chat" /></a>
+                        <a target="_blank" href="https://morning-scrubland-86999.herokuapp.com/"><img src="./images/ezchat.png" alt="EZ Chat" /></a>
                         <h5><a target="_blank" href="https://morning-scrubland-86999.herokuapp.com/">EZ Chat</a>
                             <a className="gitlink" href="https://github.com/gljacobs/ezchat" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/login.png" alt="EZ Login" /></a>
+                        <a target="_blank" href="https://ezlogin.herokuapp.com"><img src="./images/login.png" alt="EZ Login" /></a>
                         <h5><a target="_blank" href="https://ezlogin.herokuapp.com">EZ Login</a>
                             <a className="gitlink" href="https://github.com/gljacobs/simple-login/" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/santa.png" alt="Secret Santa" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/secret-santa/"><img src="./images/santa.png" alt="Secret Santa" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/secret-santa/">Secret Santa</a>
                             <a className="gitlink" href="https://github.com/gljacobs/secret-santa" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/clicky.png" alt="Clicky-Game" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/clicky-game/"><img src="./images/clicky.png" alt="Clicky-Game" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/clicky-game/">Clicky Game</a>
                             <a className="gitlink" href="https://github.com/gljacobs/clicky-game" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/drg.png" alt="Dragon Egg Game" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/dragon-egg-game/"><img src="./images/drg.png" alt="Dragon Egg Game" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/dragon-egg-game/">Dragon Egg Hunt</a>
                             <a className="gitlink" href="https://github.com/gljacobs/dragon-egg-game" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/friend.png" alt="Friend Finder" /></a>
+                        <a target="_blank" href="https://evening-lowlands-50463.herokuapp.com/"><img src="./images/friend.png" alt="Friend Finder" /></a>
                         <h5><a target="_blank" href="https://evening-lowlands-50463.herokuapp.com/">Friend Finder</a>
                             <a className="gitlink" href="https://github.com/gljacobs/friendfinder" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/hangman.png" alt="Letter Guess" /></a>
+                        <a target="_blank" href="https://github.com/gljacobs/Word-Guess"><img src="./images/hangman.png" alt="Letter Guess" /></a>
                         <h5><a target="_blank" href="https://github.com/gljacobs/Word-Guess">Node Hangman</a>
                             <a className="gitlink" href="https://github.com/gljacobs/Word-Guess" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/gymate.png" alt="Gymate" /></a>
+                        <a target="_blank" href="https://github.com/gljacobs/project2"><img src="./images/gymate.png" alt="Gymate" /></a>
                         <h5><a target="_blank" href="https://gymate123.herokuapp.com">Gymate</a>
                             <a className="gitlink" href="https://github.com/gljacobs/project2" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/gif.png" alt="GifTastic" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/GifTastic/"><img src="./images/gif.png" alt="GifTastic" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/GifTastic/">GifTastic</a>
                             <a className="gitlink" href="https://github.com/gljacobs/giftastic" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/burger.png" alt="Burger" /></a>
-                        <h5><a target="_blank" href="https://damp-lowlands-77901.herokuapp.com/?burger_name=Plain+Burger&devoured=0">Eat The Burger</a>
+                        <a target="_blank" href="https://damp-lowlands-77901.herokuapp.com/"><img src="./images/burger.png" alt="Burger" /></a>
+                        <h5><a target="_blank" href="https://damp-lowlands-77901.herokuapp.com/">Eat The Burger</a>
                             <a className="gitlink" href="https://github.com/gljacobs/burger" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/word.png" alt="Guessing Game" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/Psychic-Game/"><img src="./images/word.png" alt="Guessing Game" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/Psychic-Game/">Guessing Game</a>
                             <a className="gitlink" href="https://github.com/gljacobs/psychic-game" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
                     </div>
                     <div className="card work col">
-                        <a target="_blank" href=""><img src="./images/todo.png" alt="React ToDo" /></a>
+                        <a target="_blank" href="https://gljacobs.github.io/todolist/"><img src="./images/todo.png" alt="React ToDo" /></a>
                         <h5><a target="_blank" href="https://gljacobs.github.io/todolist/">React ToDo</a>
                             <a className="gitlink" href="https://github.com/gljacobs/todolist" target="_blank"><i className="fab fa-github-square" /></a>
                         </h5>
