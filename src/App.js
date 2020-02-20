1bab1eadfc3973f853538a17a3befc176e504268
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-02-20 15:30:34 -0800

    remove canvas bg, and readd it to react on render

--- src/App.js
+++ src/App.js
@@ -6,18 +6,33 @@ import Skills from './components/Skills';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Header />
-      <About />
-      <Skills />
-      <Gallery />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    window.VANTA.NET({
+      el: "#app",
+      mouseControls: true,
+      touchControls: true,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xf0a842,
+      backgroundColor: 0x561fb9,
+      spacing: 20.00,
+      maxDistance: 25.00,
+      points: 15.00,
+    });
+  }
+  render() {
+    return (
+      <div id="app" className="App">
+        <Navbar />
+        <Header />
+        <About />
+        <Skills />
+        <Gallery />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
